4812dd2ed3fbaf43b4c678a3759fd9efbda2825e
Savithasamraj <103304359+Savithasamraj@users.noreply.github.com>
2022-12-23 12:36:46 +0530

    commmit

--- Controllers/post.js
+++ Controllers/post.js
@@ -8,15 +8,26 @@ const  datas= async(req,res)=>{
  const  jobdetails= await data.create(req.body)
  res.status(200).send({message:"profile created"}
     )
+    let data = "This is a file containing a collection of books.";
+          
+    fs.writeFile("books.txt", data, (err) => {
+      if (err)
+        console.log(err);
+      else {
+        console.log("File written successfully\n");
+        console.log("The written has the following contents:");
+        
+      }
+    });
+
     }
     catch(error){
         console.log(error)
 
-// fs.writeFile('error.txt',data ,function (err) {
-//     if (err) return console.log(err);
-    
-//   })
 
+        
+  
+      
     }
 }
 module.exports={datas}
