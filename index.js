a3c129455df9e796db5c9ba3cc7371293c10df6f
Savithasamraj <103304359+Savithasamraj@users.noreply.github.com>
2022-12-23 11:35:56 +0530

    commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const routes=require("./Routes/jobdetails")
 mongoose.connect("mongodb+srv://savitha:Savitha19@cluster0.ngd5ggy.mongodb.net/preflight?retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology:true}).
 then(()=>console.log("connection successfull"))
 .catch((error)=>console.log(error))
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 app.use("/Tookan.api", routes)
 app.get('/', function (req, res) {
     res.send('GET request to homepage')
