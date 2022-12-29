231e0b4c00b427590786540e881dc3f57d5b20dc
Savithasamraj <103304359+Savithasamraj@users.noreply.github.com>
2022-12-29 14:37:09 +0530

    commit

--- watti.js
+++ watti.js
@@ -3,7 +3,7 @@ const axios = require('axios');
   console.log(datas,datamap)
   const options = {
     method: 'POST',
-    url: 'https://live-server-100817.wati.io/api/v1/sendTemplateMessage?whatsappNumber=916361265991',
+    url: 'https://live-server-100817.wati.io/api/v1/sendTemplateMessage?whatsappNumber=916379146848',
     headers: {
       'content-type': 'text/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YmYyZjlkMy0yMDNjLTQxZmYtOGUzNi1hOGE2NGJmMTUzMTciLCJ1bmlxdWVfbmFtZSI6ImRyaXN0aS5tYWhhbnRhQGNhcmdvZmxvLmNvIiwibmFtZWlkIjoiZHJpc3RpLm1haGFudGFAY2FyZ29mbG8uY28iLCJlbWFpbCI6ImRyaXN0aS5tYWhhbnRhQGNhcmdvZmxvLmNvIiwiYXV0aF90aW1lIjoiMTIvMjYvMjAyMiAxMDowNzoyMyIsImRiX25hbWUiOiIxMDA4MTciLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBRE1JTklTVFJBVE9SIiwiZXhwIjoyNTM0MDIzMDA4MDAsImlzcyI6IkNsYXJlX0FJIiwiYXVkIjoiQ2xhcmVfQUkifQ.-X_Ls8cKibn2V4QCXU7cy0i6rR9_f3DWiGO_9Ku3Sqw'
@@ -27,7 +27,19 @@ const axios = require('axios');
       console.log(response.data);
     })
     .catch(function (error) {
-      console.error(error);
+
+      console.log(error);
+      const data = JSON.stringify(error);
+      fs.appendFile(
+        "whatsapp" ,
+        data + "\r\n",
+        (err) => {
+          if (err) console.log(err);
+          else {
+            console.log("File written successfully");
+          }
+        }
+      );
     });
  }
 module.exports={whatsapp}
